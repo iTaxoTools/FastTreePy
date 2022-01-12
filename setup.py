acfe503a85c34f5dbe45185f32751ccdcb41ea5f
StefanPatman <stefanpatman91@gmail.com>
2022-01-12 20:41:09 +0200

    Release v0.2.0

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='fasttreepy',
-    version='0.1.dev1',
+    version='0.2.0',
     description='A Python wrapper for FastTree',
     long_description=long_description,
     long_description_content_type='text/markdown',
